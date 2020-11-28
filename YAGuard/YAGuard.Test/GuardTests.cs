2c78f3da65f922527d0ceb4f900de791dac081a4
Balint Nagy <balintn@hotmail.com>
2020-11-28 11:21:01 +0000

    Added test for alias handling.

--- YAGuard/YAGuard.Test/GuardTests.cs
+++ YAGuard/YAGuard.Test/GuardTests.cs
@@ -19,10 +19,19 @@ namespace YAGuard.Test
 
         [DataTestMethod]
         [DataRow(null)]
-        public void AgainstNull(string blah)
+        public void AgainstNull_ShouldProduceExpectedException(string badValue)
         {
-            try { Guard.AgainstNull(blah); }
-            catch (ArgumentNullException ex) { ex.Message.Should().Be("Parameter may not be null\r\nParameter name: blah"); }
+            try { Guard.AgainstNull(badValue); }
+            catch (ArgumentNullException ex) { ex.Message.Should().Be("Parameter may not be null\r\nParameter name: badValue"); }
+            catch { Assert.Fail(); }
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        public void AgainstNull_WithAlias_ShouldProduceExpectedException(string badValue)
+        {
+            try { Guard.AgainstNull(badValue, "myAlias"); }
+            catch (ArgumentNullException ex) { ex.Message.Should().Be("Parameter may not be null\r\nParameter name: myAlias"); }
             catch { Assert.Fail(); }
         }
 
